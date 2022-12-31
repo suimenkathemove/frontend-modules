66c637a12014872ce3c02a45c5ea1690b777575d
suimenkathemove <yoshiharu2580@gmail.com>
2022-12-31 20:50:27 +0900

    add stylelint-config

---
+++ modules/stylelint-config/index.js
@@ -0,0 +1,23 @@
+module.exports = {
+  plugins: ["stylelint-order"],
+  processors: [
+    [
+      "stylelint-processor-styled-components",
+      {
+        ignoreFiles: ["**/*.css", "**/*.scss"],
+      },
+    ],
+  ],
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-recess-order",
+    "stylelint-config-styled-components",
+  ],
+  // MEMO: コメントアウトを外すとメッセージは表示されなくなるがlintが効かなくなる
+  // customSyntax: '@stylelint/postcss-css-in-js',
+  rules: {
+    "string-quotes": "single",
+    "color-hex-length": "long",
+  },
+  ignoreFiles: "node_modules/**",
+};
---
+++ modules/stylelint-config/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+yarn add -D \
+  stylelint \
+  stylelint-config-recess-order \
+  stylelint-config-standard \
+  stylelint-config-styled-components \
+  stylelint-order \
+  stylelint-processor-styled-components
